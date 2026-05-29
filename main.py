0ca26a75b451265ac52e8ee380ad3c501ec309fb
daniil-knt-dev <daniilkuznetsov17021999@gmail.com>
2026-05-29 12:38:21 +0300

    Update main.py
    Что нового:
    - Возможность создания кастомных полей для проверки
    - Если проверка возвращает False, выводится причина
    - Другие мелкие улучшения и обновления

--- main.py
+++ main.py
@@ -1,75 +1,86 @@
-import json
+def validate_string(value, min_len=3, max_len=20, contains=None):
+    if type(value) != str:
+        return False, "неверный тип данных"
+    if not int(min_len) <= len(value) <= int(max_len):
+        return False, "неверная длина"
+    if contains != None:
+        if not set(contains).issubset(value):
+            return False, "неверное содержание"
+    return True
 
 
-def load():
-    try:
-        with open('data.json', 'r', encoding='utf-8') as f:
-            data = json.load(f)
-            return data
-    except:
-        return {"password": "123", "notes": []}
+def validate_int(value, min_val=1, max_val=15):
+    if type(value) != int:
+        return False, "неверный тип данных"
+    if not min_val <= value <= max_val:
+        return False, "неверное значение"
+    return True
+
+
+def validate_data(data, rules):
+    res = {}
+    for el in data:
+        if rules[el]["type"] == "str":
+            res[el] = validate_string(data[el], min_len=rules[el]["min_len"],
+                                      max_len=rules[el]["max_len"], contains=rules[el]["contains"])
+        elif rules[el]["type"] == "int":
+            res[el] = validate_int(
+                int(data[el]), int(rules[el]["min_val"]), int(rules[el]["max_val"]))
+    return res
+
+
+def set_fields_to_validate(fields):
+    fields = tuple(x.strip() for x in fields.split(","))
+    rules = {}
+    for field in fields:
+        rules.setdefault(field, {"type": None,
+                                 "min_len": None,
+                                 "max_len": None,
+                                 "contains": None,
+                                 "min_val": None,
+                                 "max_val": None
+                                 })
+    return rules
+
+
+def collect_and_save_data(rules):
+    data = {}
+    for field in rules:
+        data[field] = input(f"Введите {field}: ").strip()
+    return data
+
+
+def set_filters(rules):
+    for field in rules:
+        rules[field]["type"] = data_type = input(
+            f"Введите тип данных для {field}(str, int): ").strip().lower()
+        if data_type == "int":
+            rules[field]["min_val"] = int(
+                input(f"Введите минимальное значение {field}: ").strip())
+            rules[field]["max_val"] = int(
+                input(f"Введите максимальное значение {field}: ").strip())
+        elif data_type == "str":
+            rules[field]["min_len"] = int(
+                input(f"Введите минимальную длину {field}: ").strip())
+            rules[field]["max_len"] = int(
+                input(f"Введите максимальную длину {field}: ").strip())
+            user_input = input(
+                f"Введите (через запятую, если символов/слов несколько), "
+                f"что должно содержаться в {field}: ").strip().split(",")
+            # Удаление пробелов в словах(" яблоко" => "яблоко")
+            if len(user_input) > 1:
+                rules[field]["contains"] = [x.strip() for x in user_input]
+            elif len(user_input) == 1:
+                rules[field]["contains"] = user_input[0]
+
+
+def main():
+    rules = set_fields_to_validate(input(
+        "Введите через запятую поля для проверки(например: email, username...): ").strip())
+    set_filters(rules)
+    data = collect_and_save_data(rules)
+    print(validate_data(data, rules))
+    input("Enter для выхода")
     
-    
-def save(data):
-    try:
-        with open('data.json', 'w', encoding='utf-8') as f:
-            json.dump(data, f, ensure_ascii=False, indent=4)
-    except:
-        print('Что-то пошло не так')
-        
-        
-def add_note(data, title, text, category):
-    data['notes'].append({'title': title, 'text': text, 'category': category})
 
-
-def del_note(data, title_to_del):
-    data['notes'] = [x for x in data['notes'] if x['title'] != title_to_del]
-    
-
-def show_notes(data):
-    for i, note in enumerate(data['notes'], start=1):
-        print(f"{i}. Заголовок: {note['title']}, Текст: {note['text']}, Категория: {note['category']}")
-        
-        
-def is_correct_password(data, password):
-    status = password == data['password']
-    return status    
-
-
-def update_password(data):
-    last_password = input('Введите прошлый пароль: ')
-    if is_correct_password(data, last_password):
-        new_password = input('Введите новый пароль: ')
-        data['password'] = new_password
-        print('Пароль успешно сменён!')
-    else:
-        print('Неверно введён пароль')  
-    save(data)
-        
-
-data = load()
-
-while True:
-    password = input('🔒Введите пароль: ')
-    if is_correct_password(data, password):
-        break
-    else:
-        exit()
-
-menu = f"1. Все заметки📒\n2. Добавить заметку✏️\n3. Удалить заметку🗑️\n4. Поменять пароль🔏\n5. Выход и сохранение🚪\n> "
-while True:
-    choice = int(input(menu))
-    if choice == 1:
-        show_notes(data)
-    elif choice == 2:
-        add_note(data, input('Введите заголовок заметки: ').lower().strip(), input('Введите текст заметки: ').lower().strip(),\
-            input('Введите категорию заметки: ').lower().strip())
-    elif choice == 3:
-        del_note(data, input('Введите заголовок заметки для удаления: ').lower().strip())
-    elif choice == 4:
-        update_password(data)
-    elif choice == 5:
-        save(data)
-        break
-    else:
-        print('Некорректный ввод')
+main()
